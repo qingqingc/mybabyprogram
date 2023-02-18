dbda61a0681d6e226750ff306a890f84a50e2ffe
qingqingc <jahfyy6h>
2023-02-18 17:08:13 +0800

    good it's my option now.

--- src/MyFirstTest.java
+++ src/MyFirstTest.java
@@ -1,5 +1,7 @@
 public class MyFirstTest {
   public static void main(String[] args) {
         System.out.println("this is the third two. I have a dream.");
+        System.out.println("the first one ");
+        System.out.println("the second one ");
     }
 }
